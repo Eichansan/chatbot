4c7e0c5a39d77bb01dfc1f8a665f7f23f9b253bc
Eichansan <eisuke0327@icloud.com>
2025-07-03 23:31:09 +0900

    feat: chainlitでBasic認証実装

--- app/chainlit_app.py
+++ app/chainlit_app.py
@@ -1,8 +1,12 @@
 import chainlit as cl
 from fastapi import HTTPException
+from dotenv import load_dotenv
 
 from ai_message import generate_response
 from rag_service import rag_search
+
+load_dotenv()
+
 # チャットが開始されたときに実行される関数
 @cl.on_chat_start 
 async def on_chat_start():
@@ -24,3 +28,14 @@ async def on_message(input_message: cl.Message):
             await msg.stream_token(token)
 
     await msg.update()
+
+@cl.password_auth_callback
+def auth_callback(username: str, password: str):
+    # Fetch the user matching username from your database
+    # and compare the hashed password with the value stored in the database
+    if (username, password) == ("admin", "admin"):
+        return cl.User(
+            identifier="admin", metadata={"role": "admin"}
+        )
+    else:
+        return None
